35fe7a41e1d27eccaa5563e9d7f23b931a89cc0f
Danyil <shelofast.daniil@gmail.com>
2026-01-18 21:38:08 +0200

    correction

--- src/main/java/mate/academy/service/impl/OrderServiceImpl.java
+++ src/main/java/mate/academy/service/impl/OrderServiceImpl.java
@@ -1,6 +1,7 @@
 package mate.academy.service.impl;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 import mate.academy.dao.OrderDao;
 import mate.academy.dao.ShoppingCartDao;
@@ -8,8 +9,10 @@ import mate.academy.lib.Inject;
 import mate.academy.lib.Service;
 import mate.academy.model.Order;
 import mate.academy.model.ShoppingCart;
+import mate.academy.model.Ticket;
 import mate.academy.model.User;
 import mate.academy.service.OrderService;
+import mate.academy.service.ShoppingCartService;
 
 @Service
 public class OrderServiceImpl implements OrderService {
@@ -17,14 +20,17 @@ public class OrderServiceImpl implements OrderService {
     private OrderDao orderDao;
     @Inject
     private ShoppingCartDao shoppingCartDao;
+    @Inject
+    private ShoppingCartService shoppingCartService;
 
     @Override
     public Order completeOrder(ShoppingCart shoppingCart) {
         Order order = new Order();
-        order.setTickets(shoppingCart.getTickets());
+        ArrayList<Ticket> tickets = new ArrayList<>(shoppingCart.getTickets());
+        order.setTickets(tickets);
         order.setOrderDate(LocalDateTime.now());
         order.setUser(shoppingCart.getUser());
-        shoppingCart.getTickets().clear();
+        shoppingCartService.clearShoppingCart(shoppingCart);
         shoppingCartDao.update(shoppingCart);
         return orderDao.add(order);
     }
